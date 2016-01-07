b504d6c249d95732ca5132018ddebe47beadf2c5
Roberto Robles <robertodavidrobles@gmail.com>
2016-01-06 19:48:01 -0500

    se habilita identificacion de sentido de ingreso o salida en el carrril de entrada y la salida solo como salida (sin deteccion de entradas por carril de salida)

--- testContador.c
+++ testContador.c
@@ -21,12 +21,24 @@ boolean flag_ent=false,//indica si se realizó una entrada
 void RB_isr(void) {
   output_toggle (led0);
   datoPuerto= input_b();// leo el puerto b
-   if(bit_test(datoPuerto,4)==1&&bit_test(datoPuerto ,5)==0&&!flag_senE) flag_senE=true;
-   else if(bit_test(datoPuerto,4)==0&&bit_test(datoPuerto ,5)==1) flag_senE=false;
-   if(bit_test(datoPuerto,4)==1&&bit_test(datoPuerto ,5)==1&&flag_senE) flag_ent=true;
-   if(bit_test(datoPuerto,4)==1&&bit_test(datoPuerto ,5)==1&&!flag_senE) flag_sal=true;
+   if(bit_test(datoPuerto,4)==1&&bit_test(datoPuerto ,5)==0)
+   flag_senE=true;
+   if(bit_test(datoPuerto,4)==0&&bit_test(datoPuerto ,5)==1)
+   flag_senE=false;
+   if(bit_test(datoPuerto,4)==0&&bit_test(datoPuerto ,5)==0&&!flag_senE)
+   flag_ent=true;
+   if(bit_test(datoPuerto,4)==0&&flag_senE)
+   flag_sal=true;
 
-   if(bit_test(datoPuerto,6)==1&&bit_test(datoPuerto,7)==0) flag_sal=true;
+
+   if(bit_test(datoPuerto,6)==1&&bit_test(datoPuerto,7)==0)
+    flag_senS=true;
+    if(bit_test(datoPuerto,6)==0&&bit_test(datoPuerto,7)==1)
+     flag_senS=false;
+  if(bit_test(datoPuerto,6)==0&&bit_test(datoPuerto,7)==0&&!flag_senS){
+     flag_sal=true;
+     flag_senS=false;
+   }
 }
 
  void plazas_disponibles(boolean flag_count) {
@@ -58,13 +70,13 @@ void main()
 
   while (true){
     datoPuerto= input_b();// leo el puerto b
-   if(flag_ent==true&&bit_test(datoPuerto,5)==true){
+   if(flag_ent==true && bit_test(datoPuerto ,4)==1){
      countEnt++;
      //printf("Dato contador entrada = %u\r\n",countEnt);
      flag_ent=False;
      plazas_disponibles(false);
    }
-   if (flag_sal==true&&bit_test(datoPuerto,7)==true){
+   if (flag_sal==true&&(bit_test(datoPuerto ,5)==1||bit_test(datoPuerto ,6)==1)){
      countSal++;
      //printf("Dato contador salida = %u\r\n",countSal);
      flag_sal=False;
